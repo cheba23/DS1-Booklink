2f815ed765cabde18bc447c621a118c29f61140a
cheba23 <duducheberle23@gmail.com>
2026-08-19 19:35:43 -0300

    colocando cadastro de livros

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ from flask import Flask, render_template, request
 # Intancia do servidor do Flask
 app = Flask(__name__)
 
+lista_de_cadastro = []
+
 # Rota 1: Pagina inicial
 @app.route('/')
 def home():
@@ -16,9 +18,9 @@ def pagina_cadastro():
 # Rota 3: Status da aplicação
 @app.route('/salvar', methods= ["POST"])
 def salvar_cadastro():
-    nome_digitado = request.form.get("campo_nome")
-    info_digitada = request.form.get("campo_info")
-    return render_template("resultado.html", nome=nome_digitado, info=info_digitada)
+    livro_digitado = request.form.get("campo_livro")
+    tipoLivro_digitada = request.form.get("campo_tipoLivro")
+    return render_template("resultado.html", livro=livro_digitado, tipo=tipoLivro_digitada)
 
 if __name__=='__main__':
     app.run(debug=True)
